a20a6f80e7255de1b755fa4ebf3d25e37017a1e0
manil <iamneeken@gmail.com>
2022-03-29 21:48:32 +0545

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,9 +5,9 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'build');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(publicPath, 'index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 
 app.listen(port, () => {
